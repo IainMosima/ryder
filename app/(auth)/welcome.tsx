bee156b19a776602de1dffe6573ad78350a1fc61
Iain Mosima <iain.nyakundi@apollo.ac.ke>
2024-08-23 21:06:18 +0300

    first commit

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView } from "react-native";
+
+const OnBoarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>OnBoarding</Text>
+    </SafeAreaView>
+  );
+};
+
+export default OnBoarding;
